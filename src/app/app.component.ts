94519f69f55bf92519327cce63a44c29c19bc300
petertechy <petertechy01@gmail.com>
2025-05-01 23:32:38 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `<h1>Product Manager</h1><router-outlet />`,
 })
-export class AppComponent {
-  title = 'product-manager';
-}
+export class AppComponent {}
