3db7ad3111104b0a93c4d8195e57859310e8f622
petertechy <petertechy01@gmail.com>
2025-05-01 23:11:29 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './products/pages/product-list/product-list.component';
+import { ProductFormComponent } from './products/pages/product-form/product-form.component';
+import { ProductDetailComponent } from './products/pages/product-detail/product-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'add', component: ProductFormComponent },
+  { path: 'product/:id', component: ProductDetailComponent },
+];
