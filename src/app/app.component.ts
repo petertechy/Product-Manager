cc96b1e08d07973ebf5f4824e236c3f17b8dbc49
petertechy <petertechy01@gmail.com>
2025-05-02 20:28:29 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `<router-outlet />`,
+  imports: [RouterOutlet, NavbarComponent],
+  template: `
+  <app-navbar />
+  <div class="page-container">
+    <router-outlet></router-outlet>
+  </div>
+`,
 })
 export class AppComponent {}
