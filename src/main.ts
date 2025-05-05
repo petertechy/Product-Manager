b1faf39dfba024858d8b50c526172096e0ce3ca0
petertechy <petertechy01@gmail.com>
2025-05-05 11:48:59 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(BrowserAnimationsModule),
+    provideToastr()
+  ]
 });
