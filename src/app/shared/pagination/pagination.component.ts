fd6b247f8addc9bc67e8c101d000c79b1b861a03
petertechy <petertechy01@gmail.com>
2025-05-10 23:25:17 +0100

    Update pagination.component.ts

--- src/app/shared/pagination/pagination.component.ts
+++ src/app/shared/pagination/pagination.component.ts
@@ -10,12 +10,23 @@ import { CommonModule } from '@angular/common';
 })
 export class PaginationComponent {
   @Input() currentPage: number = 1;
-  @Input() totalPages: number = 1;
+  @Input() totalItems: number = 0;
+  @Input() itemsPerPage: number = 5;
 
   @Output() pageChanged = new EventEmitter<number>();
 
+  get totalPages(): number {
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+
+  get pageNumbers(): number[] {
+    return Array(this.totalPages)
+      .fill(0)
+      .map((_, i) => i + 1);
+  }
+
   goToPage(page: number): void {
-    if (page >= 1 && page <= this.totalPages) {
+    if (page >= 1 && page <= this.totalPages && page !== this.currentPage) {
       this.pageChanged.emit(page);
     }
   }
@@ -26,9 +37,5 @@ export class PaginationComponent {
 
   prevPage(): void {
     this.goToPage(this.currentPage - 1);
-  }
-
-  get pageNumbers(): number[] {
-    return Array(this.totalPages).fill(0).map((_, i) => i + 1);
   }
 }
